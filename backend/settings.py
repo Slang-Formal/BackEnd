d5ad0b0f95f995286f76d3930a23d078d9375d5f
Jesse Nunez <62980474+jcnunez30@users.noreply.github.com>
2023-04-20 07:43:39 -0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-biti6gg&3v)i(@-lpvzig@_nva52dzo4@)-o(z=o7lc2vr8a*6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
