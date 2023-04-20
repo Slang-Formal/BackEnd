0c6d26683781a291142ec9db63d892fa79afc78e
Jesse Nunez <62980474+jcnunez30@users.noreply.github.com>
2023-04-20 00:30:25 -0700

    Update urls.py

--- slangtranslator/urls.py
+++ slangtranslator/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('hello/', views.say_hello), 
     path('slangreturn/', views.slangreturn, name = 'slang-response'),
-    path('api/submit-string/', views.submit_string, name='submit_string'),
+    path('', views.submit_string, name='submit_string'),
 ]
